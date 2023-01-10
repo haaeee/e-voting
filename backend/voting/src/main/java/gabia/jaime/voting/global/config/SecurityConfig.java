d5e55f9169b5666cd10a83b9107c13c947b789c4
정종민 <jmchung@gabia.com>
2023-01-10 17:47:43 +0900

    feat(SecurityConfig): SecurityConfig 추가 (#5)
    Security Config filter chain 을 통해 인증 인가를 구현함
    
    이 때 admin URL은 ADMIN ROLE 존재해야 한다
    
    또한 나머지 요청은 인증된 사용자는 다 사용 가능하다

---
+++ backend/voting/src/main/java/gabia/jaime/voting/global/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package gabia.jaime.voting.global.config;
+
+import gabia.jaime.voting.domain.member.service.AuthService;
+import gabia.jaime.voting.global.exception.CustomAuthenticationEntryPoint;
+import gabia.jaime.voting.global.security.JwtTokenFilter;
+import gabia.jaime.voting.global.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final AuthService authService;
+    private final String secretKey;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public SecurityConfig(final AuthService authService,
+                          @Value("${security.jwt.secret-key}") final String secretKey,
+                          final JwtTokenProvider jwtTokenProvider) {
+        this.authService = authService;
+        this.secretKey = secretKey;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        .mvcMatchers(
+                                "/api/*/members/join",
+                                "/api/*/members/login").permitAll()
+                        .mvcMatchers("/api/*/members/join",
+                                "/api/*/members/login").permitAll()
+                        .mvcMatchers("/api/*/admins").hasRole("ROLE_ADMIN")
+                        .anyRequest().authenticated()  // hasAnyRole("ROLE_AMDIN", "ROLE_USER")
+                )
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(secretKey, authService, jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
+                .build();
+    }
+}
