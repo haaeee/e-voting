6edc78089a22abb03d131bfa623d6aad2baa9c07
정종민 <jmchung@gabia.com>
2023-01-10 10:49:54 +0900

    feat(Member): Member 도메인 구성 (#4)

---
+++ backend/voting/src/main/java/gabia/jaime/voting/domain/member/entity/Member.java
@@ -0,0 +1,95 @@
+package gabia.jaime.voting.domain.member.entity;
+
+import gabia.jaime.voting.domain.vote.entity.Vote;
+import gabia.jaime.voting.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "member", indexes = {
+        @Index(columnList = "email", unique = true),
+        @Index(columnList = "nickname", unique = true),
+        @Index(columnList = "createdAt")
+})
+@Getter @ToString(callSuper = true)
+public class Member extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "vote_right_count", nullable = false)
+    private Integer voteRightCount;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
+    private Set<Vote> votes = new LinkedHashSet<>();
+
+    protected Member() {
+    }
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Member(Long id, String email, String password, String nickname, Integer voteRightCount, Role role) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.voteRightCount = voteRightCount;
+        this.role = role;
+    }
+
+    public static Member createShareHolder(String email, String password, String nickname, Integer voteRightCount) {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .voteRightCount(voteRightCount)
+                .role(Role.SHAREHOLDER)
+                .build();
+    }
+
+    public static Member createAdministrator(String email, String password, String nickname, Integer voteRightCount) {
+        return Member.builder()
+                .email(email)
+                .password(password)
+                .nickname(nickname)
+                .voteRightCount(voteRightCount)
+                .role(Role.ADMIN)
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Member member = (Member) o;
+        return getId().equals(member.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
