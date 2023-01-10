f230e00ac3e95e4ba311c07aaf4931133f1c51a6
정종민 <jmchung@gabia.com>
2023-01-10 11:26:42 +0900

    chore(JpaConfig): JpaConfig 추가 (#4)

---
+++ backend/voting/src/main/java/gabia/jaime/voting/global/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package gabia.jaime.voting.global.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
