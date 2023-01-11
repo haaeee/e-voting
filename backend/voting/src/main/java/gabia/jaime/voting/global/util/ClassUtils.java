904ad9aa0a923a18f2b0a209f0d29a8c1cc3b679
정종민 <jmchung@gabia.com>
2023-01-11 12:56:58 +0900

    feat(ClassUtils): Authentication 에서 MemberDetails 변환 클래스 (#8)

---
+++ backend/voting/src/main/java/gabia/jaime/voting/global/util/ClassUtils.java
@@ -0,0 +1,9 @@
+package gabia.jaime.voting.global.util;
+
+
+public class ClassUtils {
+
+    public static <T> T getSafeCastInstance(Object o, Class<T> clazz) {
+        return clazz != null && clazz.isInstance(o) ? clazz.cast(o) : null;
+    }
+}
