96b77e0e7919a8e5ac68bbdaa59b021f1fa42a36
정종민 <jmchung@gabia.com>
2023-01-11 12:58:08 +0900

    feat(안건 생성): 안건을 생성하는 메서드 구현 (#8)

---
+++ backend/voting/src/main/java/gabia/jaime/voting/domain/agenda/web/AgendaAdminApiController.java
@@ -0,0 +1,37 @@
+package gabia.jaime.voting.domain.agenda.web;
+
+import gabia.jaime.voting.domain.agenda.dto.request.AgendaCreateRequest;
+import gabia.jaime.voting.domain.agenda.service.AgendaAdminService;
+import gabia.jaime.voting.global.security.MemberDetails;
+import gabia.jaime.voting.global.util.ClassUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@Slf4j
+@RestController
+public class AgendaAdminApiController {
+
+    private final AgendaAdminService agendaAdminService;
+
+    public AgendaAdminApiController(final AgendaAdminService agendaAdminService) {
+        this.agendaAdminService = agendaAdminService;
+    }
+
+    @Secured({ "ROLE_ADMIN" })
+    @PostMapping("/api/v1/agendas")
+    public ResponseEntity<Void> create(@RequestBody AgendaCreateRequest agendaCreateRequest, Authentication authentication) {
+        final MemberDetails memberDetails = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), MemberDetails.class);
+
+        log.info("{}----------------{}", agendaCreateRequest.getAgendaStatus(), agendaCreateRequest.getIssueType());
+        final Long savedId = agendaAdminService.save(memberDetails, agendaCreateRequest);
+        return ResponseEntity.created(URI.create("/api/v1/agendas/" + savedId))
+                .build();
+    }
+}
