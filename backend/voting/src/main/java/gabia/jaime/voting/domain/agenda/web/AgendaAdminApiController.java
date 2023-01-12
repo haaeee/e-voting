50adae1b46c0473d659f6422d7407bf44bd56c66
정종민 <jmchung@gabia.com>
2023-01-12 17:42:44 +0900

    feat(AgendaAdminApiController): 안건 생성 URL 변경 (#10)

--- backend/voting/src/main/java/gabia/jaime/voting/domain/agenda/web/AgendaAdminApiController.java
+++ backend/voting/src/main/java/gabia/jaime/voting/domain/agenda/web/AgendaAdminApiController.java
@@ -4,7 +4,6 @@ import gabia.jaime.voting.domain.agenda.dto.request.AgendaCreateRequest;
 import gabia.jaime.voting.domain.agenda.service.AgendaAdminService;
 import gabia.jaime.voting.global.security.MemberDetails;
 import gabia.jaime.voting.global.util.ClassUtils;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
-@Slf4j
 @RestController
 public class AgendaAdminApiController {
 
@@ -23,11 +21,9 @@ public class AgendaAdminApiController {
         this.agendaAdminService = agendaAdminService;
     }
 
-    @PostMapping("/api/v1/admin/agendas")
+    @PostMapping("/api/v1/agendas")
     public ResponseEntity<Void> create(@RequestBody AgendaCreateRequest agendaCreateRequest, Authentication authentication) {
         final MemberDetails memberDetails = ClassUtils.getSafeCastInstance(authentication.getPrincipal(), MemberDetails.class);
-
-        log.info("{}----------------{}", agendaCreateRequest.getAgendaStatus(), agendaCreateRequest.getIssueType());
         final Long savedId = agendaAdminService.save(memberDetails, agendaCreateRequest);
         return ResponseEntity.created(URI.create("/api/v1/agendas/" + savedId))
                 .build();
