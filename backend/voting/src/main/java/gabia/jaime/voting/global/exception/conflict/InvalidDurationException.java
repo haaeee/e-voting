694a7ca0242838baf28ca2b85e3f1f65a8e16770
정종민 <jmchung@gabia.com>
2023-01-16 13:17:27 +0900

    feat: Exception을 분기하여 메시지 작성  (#19)

--- backend/voting/src/main/java/gabia/jaime/voting/global/exception/conflict/InvalidDurationException.java
+++ backend/voting/src/main/java/gabia/jaime/voting/global/exception/conflict/InvalidDurationException.java
@@ -3,6 +3,6 @@ package gabia.jaime.voting.global.exception.conflict;
 public class InvalidDurationException extends ConflictException {
 
     public InvalidDurationException() {
-        super("종료 시간은 시작 시간 이후로만 설정 가능합니다.");
+        super("종료시각은 시작시각 이후로만 설정 가능합니다.");
     }
 }
